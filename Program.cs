4b2c2d99c13efcc217ebc085ae2bf30c82906e77
Mikhael101 <mikhaelbashour@gmail.com>
2026-01-22 12:52:04 +0100

    Lab1: første C# konsollapp

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        TestClass test = new TestClass();
+        test.SayHello();
+    }
+}
---
+++ TestClass.cs
@@ -0,0 +1,9 @@
+using System;
+
+class TestClass
+{
+    public void SayHello()
+    {
+        Console.WriteLine("Hello World - TestClass.cs:7");
+    }
+}
